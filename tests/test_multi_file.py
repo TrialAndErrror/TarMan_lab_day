a102718decf052a07c374ca444467fcd683c346f
Wade Green <thewadegreen@gmail.com>
2023-05-26 12:18:31 -0400

    Implement multiple file compression

--- tests/test_multi_file.py
+++ tests/test_multi_file.py
@@ -1,20 +1,19 @@
-import os
-import tarfile
 from pathlib import Path
 
 import pytest
 
-from src.compress import compress_one_file
+from src.compress import compress_one_file, compress_multiple_files
 from src.decompress import decompress
 
 
 @pytest.fixture
-def input_file(tmp_path):
-    # Create a temporary input file for testing
-    input_file_path = tmp_path / 'input.txt'
-    with open(input_file_path, 'w') as f:
-        f.write('This is a test file.')
-    return input_file_path
+def input_files(tmp_path):
+    # Create multiple temporary input files for testing
+    for num in range(10):
+        input_file_path = tmp_path / f'input_{num}.txt'
+        with open(input_file_path, 'w') as f:
+            f.write(f'This is a test file. #{num}')
+    return tmp_path.glob('*.txt')
 
 
 @pytest.fixture
@@ -23,13 +22,13 @@ def output_tarball(tmp_path):
     return tmp_path / 'output.tar.gz'
 
 
-def test_compress_and_decompress(input_file, output_tarball):
+def test_compress_and_decompress(input_files, output_tarball):
     output_dir = Path.cwd() / "test_output_files"
     output_dir.mkdir(exist_ok=True)
 
     destination_file = output_dir / "output.tar.gz"
     # Compress the input file into the tarball
-    compress_one_file(input_file, destination_file)
+    compress_multiple_files(destination_file, input_files)
 
     # Ensure the tarball exists
     assert destination_file.exists()
@@ -41,19 +40,24 @@ def test_compress_and_decompress(input_file, output_tarball):
     decompress(destination_file, output_file_dir)
 
     # Ensure the output file exists
-    output_file = output_file_dir / 'input.txt'
-    assert output_file.exists()
+    for num in range(10):
+        output_file = output_file_dir / f'input_{num}.txt'
+        assert output_file.exists()
 
     # Verify the content of the output file
-    with open(output_file, 'r') as f:
-        assert f.read() == 'This is a test file.'
+        with open(output_file, 'r') as f:
+            assert f.read() == f'This is a test file. #{num}'
 
     # Clean up temporary files
-    output_file.unlink()
+        output_file.unlink()
+        assert not output_file.exists()
+
+
     destination_file.unlink()
     output_file_dir.rmdir()
+    output_dir.rmdir()
 
     # Ensure the temporary files are deleted
-    assert not output_file.exists()
     assert not output_tarball.exists()
     assert not output_file_dir.exists()
+    assert not output_dir.exists()
---
+++ tests/test_single_file.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import pytest
+
+from src.compress import compress_one_file
+from src.decompress import decompress
+
+
+@pytest.fixture
+def input_file(tmp_path):
+    # Create a temporary input file for testing
+    input_file_path = tmp_path / 'input.txt'
+    with open(input_file_path, 'w') as f:
+        f.write('This is a test file.')
+    return input_file_path
+
+
+@pytest.fixture
+def output_tarball(tmp_path):
+    # Create a temporary output tarball for testing
+    return tmp_path / 'output.tar.gz'
+
+
+def test_compress_and_decompress(input_file, output_tarball):
+    output_dir = Path.cwd() / "test_output_files"
+    output_dir.mkdir(exist_ok=True)
+
+    destination_file = output_dir / "output.tar.gz"
+    # Compress the input file into the tarball
+    compress_one_file(input_file, destination_file)
+
+    # Ensure the tarball exists
+    assert destination_file.exists()
+
+    # Decompress the tarball into a temporary output file
+    output_file_dir = output_tarball.cwd() / output_tarball.stem
+    output_file_dir.mkdir(exist_ok=True, parents=True)
+
+    decompress(destination_file, output_file_dir)
+
+    # Ensure the output file exists
+    output_file = output_file_dir / 'input.txt'
+    assert output_file.exists()
+
+    # Verify the content of the output file
+    with open(output_file, 'r') as f:
+        assert f.read() == 'This is a test file.'
+
+    # Clean up temporary files
+    output_file.unlink()
+    destination_file.unlink()
+    output_file_dir.rmdir()
+
+    # Ensure the temporary files are deleted
+    assert not output_file.exists()
+    assert not output_tarball.exists()
+    assert not output_file_dir.exists()
